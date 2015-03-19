1165d95be16da2df033aba9dd6975ea6a8b16ca1
masterUNG <phrombutr@gmail.com>
2015-03-19 10:00:10 +0700

    Alert Dialg

--- app/src/main/java/appewtc/masterung/snruishihara/MyAlertDialog.java
+++ app/src/main/java/appewtc/masterung/snruishihara/MyAlertDialog.java
@@ -1,10 +1,29 @@
 package appewtc.masterung.snruishihara;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+
 /**
  * Created by masterUNG on 3/19/15 AD.
  */
 public class MyAlertDialog {
 
-    
+    public void showDialog(Context context) {
+
+        AlertDialog.Builder objBuilder = new AlertDialog.Builder(context);
+        objBuilder.setIcon(R.drawable.icon_question);
+        objBuilder.setTitle("Please Answer");
+        objBuilder.setMessage("Please Answer in Choice");
+        objBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        objBuilder.setCancelable(false);
+        objBuilder.show();
+
+    }
 
 }   //Main Class
